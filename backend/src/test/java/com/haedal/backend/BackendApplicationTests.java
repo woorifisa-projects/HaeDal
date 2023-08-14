f32ec7dd0b7d4e9199295a38b1e403ff5deab655
leejunehee <junehee107@naver.com>
2023-08-14 12:06:18 +0900

    Chore : 프로젝트 초기설정

---
+++ backend/src/test/java/com/haedal/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haedal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
