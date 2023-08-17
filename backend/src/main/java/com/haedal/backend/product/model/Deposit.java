303a40b0ce08f265dd80c33d539c9dfa9ce887bf
gyongyeon <126961013+Cloudyee@users.noreply.github.com>
2023-08-18 00:57:35 +0900

    Merge pull request product 컨트롤러 수정 (#25)

--- backend/src/main/java/com/haedal/backend/product/model/Deposit.java
+++ backend/src/main/java/com/haedal/backend/product/model/Deposit.java
@@ -3,6 +3,7 @@ package com.haedal.backend.product.model;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+//TODO : 상속 관련 설정
 @Entity
 @Table(name = "Deposit")
 public class Deposit extends Product {
--- backend/src/main/java/com/haedal/backend/product/model/InstallmentSavings.java
+++ backend/src/main/java/com/haedal/backend/product/model/InstallmentSavings.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+//TODO : 상속 관련 설정
 @Entity
 @Table(name = "InstallmentSavings")
 public class InstallmentSavings extends Product {
