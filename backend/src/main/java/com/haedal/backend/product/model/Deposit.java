8b21d2d632ba0d947b181ec0c8cdce4eb7126bae
image00 <119517146+image00@users.noreply.github.com>
2023-08-18 10:05:42 +0900

    Merge -  DB재설계 - Product테이블 통합 (#27)

--- backend/src/main/java/com/haedal/backend/product/model/Deposit.java
+++ backend/src/main/java/com/haedal/backend/product/model/Deposit.java
@@ -1,9 +1,11 @@
 package com.haedal.backend.product.model;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 //TODO : 상속 관련 설정
+@DiscriminatorValue("D")
 @Entity
 @Table(name = "Deposit")
 public class Deposit extends Product {
--- backend/src/main/java/com/haedal/backend/product/model/InstallmentSavings.java
+++ backend/src/main/java/com/haedal/backend/product/model/InstallmentSavings.java
@@ -1,10 +1,12 @@
 package com.haedal.backend.product.model;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 //TODO : 상속 관련 설정
+@DiscriminatorValue("I")
 @Entity
 @Table(name = "InstallmentSavings")
 public class InstallmentSavings extends Product {
