b7aec8cb07ed51ffc86abe073136e4fd0f63418e
image00 <119517146+image00@users.noreply.github.com>
2023-08-28 09:48:34 +0900

    Feature: 추천상품 조회기능 API 기능 추가 및 수정 (#78)

--- backend/src/main/java/com/haedal/backend/product/controller/ProductController.java
+++ backend/src/main/java/com/haedal/backend/product/controller/ProductController.java
@@ -22,6 +22,7 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
--- backend/src/main/java/com/haedal/backend/product/controller/RecommendedProductController.java
+++ backend/src/main/java/com/haedal/backend/product/controller/RecommendedProductController.java
@@ -30,7 +30,6 @@ public class RecommendedProductController {
 
 
     @Autowired
-
     public RecommendedProductController(RecommendedProductService recommendedProductService, SubscribeService subscribeService, UserService userService, ProfileService profileService, ProductRepository productRepository) {
         this.recommendedProductService = recommendedProductService;
         this.subscribeService = subscribeService;
@@ -49,46 +48,46 @@ public class RecommendedProductController {
      */
 
 
-//    // 자산정보에 따라 상품을 추천해주는 서비스 구현
-//    //TODO : 인증 기능 구현 후 @RequestParam("userId") Long userId 추가
-//    @GetMapping
-//    public List<ProductResponse> getRecommendedByAsset() {
-//
-//        // TODO: 접속한 사용자가 로그인했는지, 안했는지 구분하는 로직
-//
-//
-//        // userId를 기준으로, 기준을 충족하는 상품들만 저장
-//        Long userId = 1L;
-//        List<Product> recommendedByAsset = recommendedProductService.filterProductsByAsset(userId);
-//        System.out.println(recommendedByAsset);
-//
-//
-//        // 상위의 3개만 선택해서 1,2,3위 순위 보여주기
-//        // filterByAsset() : ProductRepository에서 선언한 JPQL 쿼리가 담겨있음
-//        List<Product> sortedAssets = recommendedProductService.filterByAsset(recommendedByAsset);
-//        List<Product> rankingAssets = new ArrayList<>();
-//        List<ProductResponse> productResponses = new ArrayList<>();
-//
-//        // 상위 3개의 상품을 선택
-////        int maxRankingCount = Math.max(sortedAssets.size(), 3);
-//        for (int i = 0; i < 3; i++) {
-//            if (sortedAssets.isEmpty() == false) {
-//                Product product = sortedAssets.get(i);
-//                rankingAssets.add(product);
-//
-//                // 예외처리1 : 추천상품이 없는 경우
-//            } else {
-//                System.out.println("추천상품이 없습니다");
-//            }
-//
-//
-//            productResponses = rankingAssets.stream()
-//                    .map(ProductResponse::recommendedFrom)
-//                    .collect(Collectors.toList());
-//        }
-//
-//        return productResponses;
-//    }
+    // 자산정보에 따라 상품을 추천해주는 서비스 구현
+    //TODO : 인증 기능 구현 후 @RequestParam("userId") Long userId 추가
+    @GetMapping
+    public List<ProductResponse> getRecommendedByAsset() {
+
+        // TODO: 접속한 사용자가 로그인했는지, 안했는지 구분하는 로직
+
+
+        // userId를 기준으로, 기준을 충족하는 상품들만 저장
+        Long userId = 1L;
+        List<Long> productsIdsInAsset = recommendedProductService.filterProductsByAsset(userId);
+        System.out.println(productsIdsInAsset);
+
+
+        // 상위의 3개만 선택해서 1,2,3위 순위 보여주기
+        // filterByAsset() : ProductRepository에서 선언한 JPQL 쿼리가 담겨있음
+        List<Product> sortedAssets = recommendedProductService.orderByAsset(productsIdsInAsset);
+        List<Product> rankingAssets = new ArrayList<>();
+        List<ProductResponse> productResponses = new ArrayList<>();
+
+        // 상위 3개의 상품을 선택
+//        int maxRankingCount = Math.max(sortedAssets.size(), 3);
+        for (int i = 0; i < 3; i++) {
+            if (sortedAssets.isEmpty() == false) {
+                Product product = sortedAssets.get(i);
+                rankingAssets.add(product);
+
+                // 예외처리1 : 추천상품이 없는 경우
+            } else {
+                System.out.println("추천상품이 없습니다");
+            }
+
+
+            productResponses = rankingAssets.stream()
+                    .map(ProductResponse::recommendedFrom)
+                    .collect(Collectors.toList());
+        }
+
+        return productResponses;
+    }
 
 
     // 사용자의 이용목적에 따라, 추천상품을 조회
@@ -97,11 +96,11 @@ public class RecommendedProductController {
         Long userId = 3L;
 
         // userId를 기준으로 같은 이용 목적을 가진 상품들의 productId 목록을 가져옴
-        List<Long> recommendedProductIds = recommendedProductService.filterProductsByServicePurpose(userId);
-        System.out.println("찾은 목록 아이디들 : " + recommendedProductIds);
+        List<Long> productIdsByServicePurpose = recommendedProductService.filterProductsByServicePurpose(userId);
+        System.out.println("찾은 목록 아이디들 : " + productIdsByServicePurpose);
 
         // 기준 충족 상품을 정렬
-        List<Product> sortedServicePurpose = recommendedProductService.filterByServicePurpose(recommendedProductIds);
+        List<Product> sortedServicePurpose = recommendedProductService.orderByServicePurpose(productIdsByServicePurpose);
         System.out.println("목록들 정렬 : " + sortedServicePurpose);
 
         // 인기 순위 상위 3개의 상품 저장
@@ -164,7 +163,54 @@ public class RecommendedProductController {
 //        return productResponses;
 //    }
 
-//
+
+
+
+    // 사용자의 연령대에 따라, 추천상품을 조회
+    @GetMapping("/filter/userAgeGroup")
+    public List<ProductResponse> getRecommendedByUserAgeGroup() {
+        Long userId = 1L;
+
+        // userId를 기준으로 같은 연령대를 가진 상품들의 productId 목록을 가져옴
+        List<Long> productIdsInUserAgeGroup = recommendedProductService.filterProductsByUserAgeGroup(userId);
+        System.out.println("찾은 목록 아이디들 : " + productIdsInUserAgeGroup);
+        System.out.println("찾은 목록 아이디들 : " + recommendedProductService.filterProductsByUserAgeGroup(userId));
+
+        // 기준 충족 상품을 정렬
+        List<Product> sortedServicePurpose = recommendedProductService.orderByUserAgeGroup(productIdsInUserAgeGroup);
+        System.out.println("목록들 정렬 : " + sortedServicePurpose);
+
+        // 인기 순위 상위 3개의 상품 저장
+        List<Product> rankingServicePurpose = new ArrayList<>();
+
+        if (sortedServicePurpose.size()>=3) {
+            for (int i = 0; i < 3 ; i++) {
+                Product product = sortedServicePurpose.get(i);
+                rankingServicePurpose.add(product);
+            }
+        } else if(sortedServicePurpose.size()<3) {
+            for (int i = 0;  i < sortedServicePurpose.size(); i++) {
+                Product product = sortedServicePurpose.get(i);
+                rankingServicePurpose.add(product);
+            }
+        }
+        else if(sortedServicePurpose.size()==0) {
+            System.out.println("추천상품이 없습니다");
+        }
+
+
+        // 상품을 ProductResponse로 변환하는 작업
+        List<ProductResponse> productResponses = rankingServicePurpose.stream()
+                .map(ProductResponse::recommendedFrom)
+                .collect(Collectors.toList());
+
+        return productResponses;
+    }
+
+
+
+
+
 //    // 사용자의 연령대에 따라, 추천상품을 조회
 //    @GetMapping("/filter/userAgeGroup")
 //    public List<ProductResponse> getRecommendedByUserAgeGroup() {
@@ -175,9 +221,10 @@ public class RecommendedProductController {
 //
 //        // 정렬된 상품 기준, 구독자 수에 따라 정렬
 //        // filterByUserAgeGroup() : ProductRepository에서 선언한 JPQL 쿼리가 담겨있음
-//        List<Product> sortedUserAgeGroup = productRepository.filterByUserAgeGroup(recommendedByUserAgeGroup);
+//        List<Product> sortedUserAgeGroup = productRepository.orderByUserAgeGroup(recommendedByUserAgeGroup);
 //        List<Product> rankingUserAgeGroup = new ArrayList<>();
 //        List<ProductResponse> productResponses = new ArrayList<>();
+//
 //
 //
 //        // 상위 3개의 상품을 선택
