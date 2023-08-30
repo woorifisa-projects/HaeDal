e1bd5e745c06a9e65490fa967c96df26fe4345d7
leejunehee <junehee107@naver.com>
2023-08-30 20:27:01 +0900

    Feat : 구독 리스트 화면 개발 및 상품 상세 정보 추가

--- frontend/src/components/ProfileNavigationBar.vue
+++ frontend/src/components/ProfileNavigationBar.vue
@@ -9,7 +9,7 @@
         <v-list color="transparent">
           <v-list-item prepend-icon="mdi-account-box" title="내 프로필" href="#"></v-list-item>
           <v-list-item prepend-icon="mdi-view-dashboard" title="보안 설정" href="/securitysetting"></v-list-item>
-          <v-list-item prepend-icon="mdi-gavel" title="계좌 관리" href="#"></v-list-item>
+          <v-list-item prepend-icon="mdi-gavel" title="계좌 관리" href="/portfolio"></v-list-item>
         </v-list>
 
         <template v-slot:append>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -56,7 +56,20 @@ const routes = [
         name: 'subscribeD',
         component: () => import('@/views/SubscribeD.vue'),
         props: true
-      }, {
+      },
+      {
+        path: '/subscribe/:id/IforShow',
+        name: 'subscribeIforShow',
+        component: () => import('@/views/SubscribeIforShow.vue'),
+        props: true
+      },
+      {
+        path: '/subscribe/:id/DforShow',
+        name: 'subscribeDforShow',
+        component: () => import('@/views/SubscribeDforShow.vue'),
+        props: true
+      },
+       {
         path: '/success',
         name: 'success',
         component: () => import('@/views/SubscribeSuccess'),
@@ -99,25 +112,11 @@ const routes = [
         component: () => import('@/views/ChangePassword.vue')
       },
       {
-        path: '/profile/edit',
-        name: 'profile',
-        component: () => import('@/views/UserProfileEdit.vue')
+        path: '/portfolio',
+        name: 'portfolio',
+        component: () => import('@/views/Portfolio.vue')
       },
-      {
-        path: '/security',
-        name: 'security',
-        component: () => import('@/views/SecuritySetting.vue')
-      },
-      {
-        path: '/login',
-        name: 'login',
-        component: () => import('@/views/LoginView.vue')
-      },
-      {
-        path: '/securitysetting',
-        name: 'securitySetting',
-        component: () => import('@/views/ForSecuritySetting.vue')
-      },
+      
     ],
   }
 ]
