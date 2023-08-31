eb8f3b822408d69e385f5cdc7c554def709b85a1
leejunehee <junehee107@naver.com>
2023-08-31 10:57:13 +0900

    Refactor : main과의 pull작업시 충돌 병합

--- frontend/src/components/ProfileNavigationBar.vue
+++ frontend/src/components/ProfileNavigationBar.vue
@@ -1,9 +1,9 @@
 <template>
   <v-navigation-drawer style="background: rgba(0, 162, 255, 0.088);" theme="ligth" permanent minimize>
     <v-list color="transparent">
-      <v-list-item prepend-icon="mdi-account-box" title="내 프로필" href="#"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-box" title="내 프로필" href="/mypage"></v-list-item>
       <v-list-item prepend-icon="mdi-view-dashboard" title="보안 설정" href="/securitysetting"></v-list-item>
-      <v-list-item prepend-icon="mdi-gavel" title="계좌 관리" href="#"></v-list-item>
+      <v-list-item prepend-icon="mdi-gavel" title="계좌 관리" href="/portfolio"></v-list-item>
     </v-list>
 
     <template v-slot:append>
