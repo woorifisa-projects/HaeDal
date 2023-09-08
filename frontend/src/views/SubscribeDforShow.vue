7d987d456589c8dc68df2416fa195931bb71ec83
juneheel <junehee107@naver.com>
2023-09-09 00:18:26 +0900

    Feat : 상품해지 api개발 및 프론트엔드 화면 수정

--- frontend/src/views/SubscribeDforShow.vue
+++ frontend/src/views/SubscribeDforShow.vue
@@ -64,10 +64,6 @@
                         </div>                   
 
                         <div>
-                            <b><label for="startMoney">예상 환급액 </label>{{ listData.productAsset }}원</b>
-                        </div>
-
-                        <div>
                             <b>인증 번호</b>
                             <input type="text" id="authenticationNumber" v-model="formData.authenticationNumber" required>
                         </div>
@@ -82,6 +78,25 @@
                     
                 </div>
             </div>
+            
+            <div v-if="showfinalModal" class="blur-background" @click="closeModal"></div>
+                <div v-if="showfinalModal" class="finalmodal">
+                    <!-- 모달 내용: 해지완료 -->
+                    <div style="margin:100px">
+                    <v-card class="success-card">
+                        <h1>상품 해지가 완료되었습니다</h1>
+                        <div class=" success">
+                        <img src='@/assets/img/leave.jpg'>
+                        </div>
+                        <h1>{{ result }}</h1>
+                        <p style="margin-bottom: 18px;font-size: 13px;">해달과 계속해서 저축해요...</p>
+                        <v-btn class="btn" @click="goportfolio">확인</v-btn>
+                    </v-card>
+                    <div class=" overlay">
+                    </div>
+                </div>
+            </div>
+
 
         </form>
     </div>
@@ -105,8 +120,8 @@ const axiosInstance = axios.create({
 })
 
 const listData = ref({});
-const calculatedAmount = ref(null);
-
+const result = ref(0); //환급액
+const showfinalModal = ref(false);
 const route = useRoute();
 const productId = route.params.id;
 const currentPath = `/subscribe/${productId}`;
@@ -128,11 +143,12 @@ const formData = {
     startMoney: ''
 };
 
+const goportfolio = () => {
+    router.push('/portfolio');
+}
 
 const submitForm = () => {
     const url = `http://localhost:8080/subscribe/${productId}/cancle`;
-
-
 // productId가 유효한 경우에만 요청을 보냅니다.
 axios.delete(url,
     {
@@ -141,6 +157,11 @@ axios.delete(url,
         }
     },)
     .then((res) => {
+    showModal.value = false;
+    showfinalModal.value = true;
+    result.value = res.data;
+    console.log(res);
+    
     console.log(res);
     
 }).catch((error) => {
@@ -269,6 +290,20 @@ form {
     overflow: hidden;
 }
 
+.finalmodal {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 9999;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+    max-width: 80%;
+    width: 50rem;
+    overflow: hidden;
+}
+
 /* 블러 효과 스타일 */
 .blur-background {
     position: fixed;
@@ -306,6 +341,31 @@ form {
     width: 25px;
     height: 25px;
     object-fit: cover;
+}
+.success img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.success-card {
+  box-shadow: -4px 4px 10px 0 rgba(51, 96, 133, 0.252);
+  width: 100%;
+  text-align: center;
+  padding: 40px;
+  margin: auto;
+  z-index: 2;
+}
+
+.btn {
+  width: 8rem;
+  border-radius: 8px;
+  box-shadow: none;
+  background: rgba(0, 179, 255, 0.826);
+  color: white;
+  margin: 8px;
+  font-weight: bolder;
+  font-size: 16px;
 }
 </style>
   
--- frontend/src/views/SubscribeIforShow.vue
+++ frontend/src/views/SubscribeIforShow.vue
@@ -42,12 +42,28 @@
                     <!-- 모달 내용: 계좌번호와 인증 번호 입력 -->
                     <div class="modal-content">
                         <h2>{{ listData.productName }} 상품 해지</h2>
+                        <v-icon color="error">mdi-alert-circle</v-icon>
+                        <div class="warning-message">
+                        <p>
+                            ⚠ 중도 해지 시 투자 원금만 반환됩니다 ⚠
+                        </p>
+                        <p>
+                            고객님,
+
+        저희 금융 상품에 투자해 주셔서 감사합니다. 중요한 안내를 드립니다.
+
+        금융 상품 중도 해지 시, 투자 원금만을 반환받으실 수 있습니다. 이것은 해당 상품의 특성에 따른 것으로, 중도 해지 시 수익을 포함한 추가 금액을 받지 못하게 됩니다. 따라서 투자 기간이 끝나지 않았을 경우, 원금의 일부 손실이 발생할 수 있습니다.
+
+        중요한 결정을 하기 전에 모든 옵션을 신중히 고려하시기 바랍니다. 투자 상품에 대한 이해를 높이고, 본인의 금융 목표를 달성하기 위해 최선의 선택을 하실 수 있도록 노력하겠습니다.
+
+        감사합니다.
+                        </p>
+                        </div>                   
+
+
+                        <div>
 
                     
-                        <div>
-                            <b><label for="startMoney">예상 환급액 </label>{{ listData.productAsset }}원</b>
-                        </div>
-
                         <div>
                             <b>인증 번호</b>
                             <input type="text" id="authenticationNumber" v-model="formData.authenticationNumber" required>
@@ -62,6 +78,25 @@
                         </div>
                     </div>
                 </div>
+            </div>
+                
+            <div v-if="showfinalModal" class="blur-background" @click="closeModal"></div>
+                <div v-if="showfinalModal" class="finalmodal">
+                    <!-- 모달 내용: 해지완료 -->
+                    <div style="margin:100px">
+                    <v-card class="success-card">
+                        <h1>상품 해지가 완료되었습니다</h1>
+                        <div class=" success">
+                        <img src='@/assets/img/leave.jpg'>
+                        </div>
+                        <h1>{{ result }}</h1>
+                        <p style="margin-bottom: 18px;font-size: 13px;">해달과 계속해서 저축해요...</p>
+                        <v-btn class="btn" @click="goportfolio">확인</v-btn>
+                    </v-card>
+                    <div class=" overlay">
+                    </div>
+                </div>
+            </div>
 
         </form>
     </div>
@@ -83,9 +118,8 @@ const axiosInstance = axios.create({
 const authStore = useAuthStore();
 const listData = ref({});
 const showModal = ref(false);
-
-const calculatedAmount = ref(null);
-
+const showfinalModal = ref(false);
+const result = ref(0); //환급액
 const route = useRoute();
 const productId = route.params.id;
 const currentPath = `/subscribe/${productId}`;
@@ -103,16 +137,17 @@ watchEffect(() => {
 
 console.log(listData);
 
+const goportfolio = () => {
+    router.push('/portfolio');
+}
+
 const formData = {
     authenticationNumber: '',
     startMoney: ''
 };
 
-
-
 const submitForm = () => {
     const url = `http://localhost:8080/subscribe/${productId}/cancle`;
-
 
 // productId가 유효한 경우에만 요청을 보냅니다.
 axios.delete(url,
@@ -122,6 +157,9 @@ axios.delete(url,
         }
     },)
     .then((res) => {
+    showModal.value = false;
+    showfinalModal.value = true;
+    result.value = res.data;
     console.log(res);
     
 }).catch((error) => {
@@ -138,17 +176,40 @@ const closeModal = () => {
     showModal.value = false;
 };
 
+const openfinalModal = () => {
+    showfinalModal.value = true;
+   
+};
+
+const closefinalModal = () => {
+    showfinalModal.value = false;
+};
+
+
 </script>
   
  
 <style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@500&display=swap');
 
+
+.warning-message {
+  background-color: #ffcccc;
+  border: 1px solid #ff3333;
+  padding: 10px;
+  text-align: center;
+  font-weight: bold;
+  color: #ff3333;
+}
+
+
 .error-icon {
     width: 30%;
     margin: auto;
     margin-top: 5px;
 }
+
+
 
 .error-icon img {
     width: 100%;
@@ -241,6 +302,20 @@ form {
     overflow: hidden;
 }
 
+.finalmodal {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 9999;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+    max-width: 80%;
+    width: 50rem;
+    overflow: hidden;
+}
+
 /* 블러 효과 스타일 */
 .blur-background {
     position: fixed;
@@ -278,6 +353,41 @@ form {
     width: 25px;
     height: 25px;
     object-fit: cover;
+}
+
+.success {
+  width: 200px;
+  height: 200px;
+  border-radius: 70%;
+  overflow: hidden;
+  margin: auto;
+  margin-bottom: 30px;
+}
+
+.success img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.success-card {
+  box-shadow: -4px 4px 10px 0 rgba(51, 96, 133, 0.252);
+  width: 100%;
+  text-align: center;
+  padding: 40px;
+  margin: auto;
+  z-index: 2;
+}
+
+.btn {
+  width: 8rem;
+  border-radius: 8px;
+  box-shadow: none;
+  background: rgba(0, 179, 255, 0.826);
+  color: white;
+  margin: 8px;
+  font-weight: bolder;
+  font-size: 16px;
 }
 </style>
   
