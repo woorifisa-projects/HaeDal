82987daac3f2752575034035476aeef134597d77
leejunehee <junehee107@naver.com>
2023-09-12 14:31:26 +0900

    feat : profile 단위테스트 코드 작성 및 ProfileController 코드 리팩토링

--- backend/src/main/java/com/haedal/backend/profile/service/ProfileService.java
+++ backend/src/main/java/com/haedal/backend/profile/service/ProfileService.java
@@ -4,8 +4,7 @@ package com.haedal.backend.profile.service;
 import com.haedal.backend.auth.dto.UserDto;
 import com.haedal.backend.auth.dto.request.UserUpdateRequest;
 import com.haedal.backend.auth.model.User;
-
-import java.util.Optional;
+import com.haedal.backend.profile.dto.response.ProfileResponse;
 
 
 public interface ProfileService extends CrudService<User,Long>{
@@ -19,4 +18,6 @@ public interface ProfileService extends CrudService<User,Long>{
     User updatePassword(User user, String password);
 
     Long getUserId(User user);
+
+    ProfileResponse userUpdateProfile(User user, UserUpdateRequest userUpdateRequest);
 }
