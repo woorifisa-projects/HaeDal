f746bdbe150ad1f8e4c85d6ca46e06eb5a48dc13
gyongyeon <126961013+Cloudyee@users.noreply.github.com>
2023-09-14 17:27:24 +0900

    Update RecommendedProductService.java

--- backend/src/main/java/com/haedal/backend/product/service/RecommendedProductService.java
+++ backend/src/main/java/com/haedal/backend/product/service/RecommendedProductService.java
@@ -16,6 +16,38 @@ public interface RecommendedProductService extends CrudService<Product, Long> {
 
     // 이용목적에 따른 추천상품 전체 조회(상위 랭킹 반영 전)
     List<Product> filterProductsByServicePurpose(Long userId);
+package com.haedal.backend.product.service;
+
+import com.haedal.backend.product.model.Product;
+
+import java.util.List;
+
+
+public interface RecommendedProductService extends CrudService<Product, Long> {
+
+//
+//    // 자산에 따른 추천상품 전체 조회(상위 랭킹 반영 전)
+    List<Long> filterProductsByAsset(Long userId);
+
+    // 이용목적에 따른 추천상품 전체 조회(상위 랭킹 반영 전)
+    List<Long> filterProductsByServicePurpose(Long userId);
+
+//    // 연령대에 따른 추천상품 전체 조회(상위 랭킹 반영 전)
+    List<Long> filterProductsByUserAgeGroup(Long userId);
+//
+
+
+    List<Product> orderByAsset(List<Long> recommendedProductIdsByAsset);
+
+
+    List<Product> orderByServicePurpose(List<Long> recommendedProductIdsByServicePurpose);
+
+
+    List<Product> orderByUserAgeGroup(List<Long> recommendedProductsIdsByUserAgeGroup);
+
+    List<Product> orderByInterestRate(List<Product> products);
+
+}
 
 //    // 연령대에 따른 추천상품 전체 조회(상위 랭킹 반영 전)
     List<Product> filterProductsByUserAgeGroup(Long userId);
