491e7caa5f630d9e4264dadb84b67df0abf2e32c
gyongyeon <126961013+Cloudyee@users.noreply.github.com>
2023-09-14 17:22:14 +0900

    fix : 이전버전

--- backend/src/main/java/com/haedal/backend/auth/config/SecurityConfig.java
+++ backend/src/main/java/com/haedal/backend/auth/config/SecurityConfig.java
@@ -9,11 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -66,9 +62,6 @@ public class SecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
-
 }
 
 
