4c20a1926c32d7f868bd1d84a503aa35fb84846f
leejunehee <junehee107@naver.com>
2023-09-15 09:46:15 +0900

    fix : 안쓰는페이지 삭제 - 배포 오류 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,146 +1,143 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '',
-        redirect: '/home'
+        path: "",
+        redirect: "/home",
       },
       {
-        path: '/home',
-        name: 'Home',
+        path: "/home",
+        name: "Home",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/MainPage.vue'),
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/MainPage.vue"),
       },
       {
         path: "/signup",
         name: "Signup",
-        component: () => import(/* webpackChunkName: "home" */ '@/views/SignUpView.vue'),
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/SignUpView.vue"),
       },
       {
-        path: '/products',
-        name: 'product_list',
-        component: () => import('@/views/ProductList.vue'),
-        props: true
+        path: "/products",
+        name: "product_list",
+        component: () => import("@/views/ProductList.vue"),
+        props: true,
       },
       {
-        path: '/subscribe/:id/I',
-        name: 'subscribeI',
-        component: () => import('@/views/SubscribeI.vue'),
-        props: true
+        path: "/subscribe/:id/I",
+        name: "subscribeI",
+        component: () => import("@/views/SubscribeI.vue"),
+        props: true,
       },
       {
-        path: '/subscribe/:id/D',
-        name: 'subscribeD',
-        component: () => import('@/views/SubscribeD.vue'),
-        props: true
+        path: "/subscribe/:id/D",
+        name: "subscribeD",
+        component: () => import("@/views/SubscribeD.vue"),
+        props: true,
       },
       {
-        path: '/subscribe/:id/IforShow',
-        name: 'subscribeIforShow',
-        component: () => import('@/views/SubscribeIforShow.vue'),
-        props: true
+        path: "/subscribe/:id/IforShow",
+        name: "subscribeIforShow",
+        component: () => import("@/views/SubscribeIforShow.vue"),
+        props: true,
       },
       {
-        path: '/subscribe/:id/DforShow',
-        name: 'subscribeDforShow',
-        component: () => import('@/views/SubscribeDforShow.vue'),
-        props: true
+        path: "/subscribe/:id/DforShow",
+        name: "subscribeDforShow",
+        component: () => import("@/views/SubscribeDforShow.vue"),
+        props: true,
       },
       {
-        path: '/recommend',
-        name: 'recommended_product_list',
-        component: () => import('@/views/RecommendedProduct.vue'),
-        props: true
+        path: "/recommend",
+        name: "recommended_product_list",
+        component: () => import("@/views/RecommendedProduct.vue"),
+        props: true,
       },
       {
-        path: '/mypage',
-        name: 'Mypage',
-        component: () => import('@/views/MyPage.vue')
+        path: "/mypage",
+        name: "Mypage",
+        component: () => import("@/views/MyPage.vue"),
       },
       {
-        path: '/profile/edit',
-        name: 'profile',
-        component: () => import('@/views/UserProfileEdit.vue')
+        path: "/profile/edit",
+        name: "profile",
+        component: () => import("@/views/UserProfileEdit.vue"),
       },
       {
-        path: '/security',
-        name: 'security',
-        component: () => import('@/views/SecuritySetting.vue')
+        path: "/security",
+        name: "security",
+        component: () => import("@/views/SecuritySetting.vue"),
       },
       {
-        path: '/login',
-        name: 'login',
-        component: () => import('@/views/LoginView.vue')
+        path: "/login",
+        name: "login",
+        component: () => import("@/views/LoginView.vue"),
       },
       {
-        path: '/securitysetting',
-        name: 'securitySetting',
-        component: () => import('@/views/ForSecuritySetting.vue')
+        path: "/securitysetting",
+        name: "securitySetting",
+        component: () => import("@/views/ForSecuritySetting.vue"),
       },
       {
-        path: '/security',
-        name: 'security',
-        component: () => import('@/views/SecurityMain.vue')
+        path: "/security",
+        name: "security",
+        component: () => import("@/views/SecurityMain.vue"),
       },
       {
-        path: '/changePW',
-        name: 'changepassword',
-        component: () => import('@/views/ForPasswordChange.vue')
+        path: "/changePW",
+        name: "changepassword",
+        component: () => import("@/views/ForPasswordChange.vue"),
       },
       {
-        path: '/portfolio',
-        name: 'portfolio',
-        component: () => import('@/views/Portfolio.vue')
+        path: "/portfolio",
+        name: "portfolio",
+        component: () => import("@/views/Portfolio.vue"),
       },
       {
-        path: '/portfoliodays',
-        name: 'portfoliodays',
-        component: () => import('@/views/PortfolioDays.vue')
+        path: "/portfoliodays",
+        name: "portfoliodays",
+        component: () => import("@/views/PortfolioDays.vue"),
       },
       {
-        path: '/portfoliodibs',
-        name: 'portfoliodibs',
-        component: () => import('@/views/Portfoliodibs.vue')
+        path: "/portfoliodibs",
+        name: "portfoliodibs",
+        component: () => import("@/views/Portfoliodibs.vue"),
       },
       {
-        path: '/chartportfolio',
-        name: 'chartportfolio',
-        component: () => import('@/views/ChartPortfolio.vue')
+        path: "/test",
+        name: "test",
+        component: () => import("@/views/Test"),
       },
       {
-        path: '/test',
-        name: 'test',
-        component: () => import('@/views/Test'),
+        path: "/leave",
+        name: "leave",
+        component: () => import("@/views/UserLeave"),
       },
       {
-        path: '/leave',
-        name: 'leave',
-        component: () => import('@/views/UserLeave'),
+        path: "/error",
+        name: "error",
+        component: () => import("@/views/Error"),
       },
-      {
-        path: '/error',
-        name: 'error',
-        component: () => import('@/views/Error'),
-      }
-    ]
+    ],
   },
   {
-    path: '/success',
-    name: 'success',
-    component: () => import('@/views/SubscribeSuccess'),
-  }
-]
+    path: "/success",
+    name: "success",
+    component: () => import("@/views/SubscribeSuccess"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
